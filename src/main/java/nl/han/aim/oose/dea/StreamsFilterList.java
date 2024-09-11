b98e65bb80fcd569ac48edb3ae46fc77298680b8
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2024-09-11 15:54:23 +0200

    HATSAA
    boomba

--- src/main/java/nl/han/aim/oose/dea/StreamsFilterList.java
+++ src/main/java/nl/han/aim/oose/dea/StreamsFilterList.java
@@ -1,5 +1,6 @@
 package nl.han.aim.oose.dea;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,12 +9,19 @@ public class StreamsFilterList {
     List<String> filterStringsShorterThanThreeCharacters(List<String> input) {
         // TODO: implement
 
-        return input;
+        return input
+                .stream()
+                .filter(s -> s.length() < 3)
+                .toList();
+
     }
 
     List<String> filterStringsThatContainOnlyNumerals(List<String> input) {
         // TODO: implement
 
-        return input;
+        return input
+                .stream()
+                .filter(s -> s.matches("[0-9]+"))
+                .toList();
     }
 }
